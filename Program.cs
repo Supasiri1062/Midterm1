cd4a9d69cfd8318d63ed9947e9bb71d9747259b7
Supasiri <supasiri.benj@mail.kmutt.ac.th>
2023-03-13 17:28:06 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class program{
+    static void Main(string[] args) {
+        Console.Write("Enter the Day: ");
+        int day = int.Parse(Console.ReadLine());
+        if(day >=1 && day <= 7){
+            Console.Write("Enter the time: ");
+            int time = int.Parse(Console.ReadLine());
+            if(time >= 8 && time <= 18){
+                Console.Write("Enter the Menu: ");
+                string menu = (Console.ReadLine());
+                while(menu == "Breakfast set"){
+                   
+                    
+                }
+                    
+            }
+            
+
+        }
+                            
+    }
+}
